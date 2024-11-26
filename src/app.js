d282999e381721c6f4302e59f2fa19401e1d5168
Mimoun El Yazidi <87827134+min45@users.noreply.github.com>
2024-11-26 17:27:06 +0000

    random card generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const simbolos = ["♥", "♦", "♠", "♣"];
+const valores = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+function generarCarta() {
+  const simbolo = simbolos[Math.floor(Math.random() * simbolos.length)];
+  const valor = valores[Math.floor(Math.random() * valores.length)];
+
+  const cartaDiv = document.getElementById("carta");
+  const topSymbol = document.getElementById("topSymbol");
+  const bottomSymbol = document.getElementById("bottomSymbol");
+  const valorDiv = document.getElementById("valor");
+
+  topSymbol.innerText = simbolo;
+  bottomSymbol.innerText = simbolo;
+  valorDiv.innerText = valor;
+
+  cartaDiv.classList.remove("corazones", "diamantes", "picas", "treboles");
+  if (simbolo === "♥" || simbolo === "♦") {
+    cartaDiv.classList.add(simbolo === "♥" ? "corazones" : "diamantes");
+  } else {
+    cartaDiv.classList.add(simbolo === "♠" ? "picas" : "treboles");
+  }
+}
+
+window.onload = generarCarta;
